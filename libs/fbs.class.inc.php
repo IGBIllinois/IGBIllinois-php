b770661991b5df0d0e2c6ad51b35409bc0c6a816
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2023-03-27 15:14:39 -0500

    More fbs class updates have post and get methods

--- libs/fbs.class.inc.php
+++ libs/fbs.class.inc.php
@@ -35,6 +35,9 @@ class fbs {
 	/** @var string secret key */
 	private $secret_key;
 
+	/** @var string token key */
+	private $token;
+
 	/**
 	 * Creates fbs object
 	 * @param string $access_key API Access Key to connect to FBS Service
@@ -45,7 +48,6 @@ class fbs {
 	public function __construct($access_key,$secret_key) {
 		$this->access_key = $access_key;
 		$this->secret_key = $secret_key;
-		$this->debug = $debug;
 	}
 
         /**
@@ -58,24 +60,62 @@ class fbs {
 
         }
 
+	public function login() {
+                $login_array = array('AccessKeyId'=>$this->access_key,
+                                'SecretAccessKey'=>$this->secret_key
+                        );
+                $login_json = json_encode($login_array);
+                $url = self::FBS_URL . self::LOGIN;
+                try {
+                        $result = $this->post($url,$login_json);
+
+                }
+                catch (\Exception $e) {
+                        throw $e;
+                        return false;
+                }
+                $response = json_decode($result,true);
+                if (isset($response['Token'])) {
+                        $this->token = $response['Token'];
+                        return true;
+                }
+                return false;
+
+        }
+
+        public function get_customers($facility_id) {
+                $header = array('Authorization: Bearer ' . $this->token);
+                $url = self::FBS_URL . "/FBS/Facility/" . $facility_id . "/Customer";
+                try {
+                        $result = $this->get($url,$header);
+
+                }
+                catch (\Exception $e) {
+                        throw $e;
+                        return false;
+                }
+                $response = json_decode($result,true);
+                return $response;
+
+        }
 	
 	/*
 	 * Sends json string to FBS rest api
 	 *
+	 * @param url $url url to contact
 	 * @param json $json_payload json formatted string
 	 *
 	 * @throws \Exception
 	 * @return json json response
 	 */
-	private function send_curl($json_payload) {
+	private function post($url,$json_payload,$additional_headers = array()) {
 		$headers = array(
 			'Accept: ' . self::HEADER_ACCEPT,
 			'Content-Type: ' . self::HEADER_CONTENT_TYPE,
-			'Cache-Control: no-cache',
-			'Ocp-Apim-Subscription-Key: ' . $this->api_key
+			'Cache-Control: no-cache'
 		);
+		$headers = array_merge($headers,$additional_headers);
 	
-		$url = self::FBS_URL . self::VALIDATE_ELEMENTS;
 		$this->ch = curl_init($url);
 		if (!is_resource($this->ch)) {
 			throw new \Exception('Curl did not init');
@@ -83,6 +123,7 @@ class fbs {
 		}
 		$this->set_curl_settings();
 		curl_setopt($this->ch, CURLOPT_URL, $url);
+		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($this->ch, CURLOPT_POSTFIELDS, $json_payload );
 		curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers);
 
@@ -104,6 +145,43 @@ class fbs {
 
 	}
 
+	private function get($url,$additional_headers) {
+                $headers = array(
+                        'Accept: ' . self::HEADER_ACCEPT,
+                        'Content-Type: ' . self::HEADER_CONTENT_TYPE,
+                        'Cache-Control: no-cache'
+                );
+                $headers = array_merge($headers,$additional_headers);
+                $this->ch = curl_init($url);
+                if (!is_resource($this->ch)) {
+                        throw new \Exception('Curl did not init');
+                        return false;
+                }
+		
+		$this->set_curl_settings();
+                curl_setopt($this->ch, CURLOPT_URL, $url);
+                curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "GET");
+                curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true );
+	
+                if (! $result = curl_exec($this->ch)) {
+                      throw new \Exception('Error sending curl');
+                	return false;
+
+                }
+                $httpcode = curl_getinfo($this->ch,CURLINFO_HTTP_CODE);
+                if ($httpcode != 200) {
+                        throw new \Exception('Bad response: ' . $httpcode);
+                        return false;
+                }
+                //curl_close is only valid for php versions less than 8
+                if (version_compare(PHP_VERSION,'8.0.0', '<') && is_resource($this->ch)) {
+                        curl_close($this->ch);
+                }
+                return $result;
+
+        }
+
 	/**
 	 * Sets Curl Settings
 	 *
@@ -111,7 +189,6 @@ class fbs {
 	 * @return vaoid
 	 */
 	private function set_curl_settings() {
-		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true );
 		curl_setopt($this->ch, CURLOPT_TIMEOUT, 10);
 
@@ -119,15 +196,6 @@ class fbs {
 
 	}
 
-	private function login() {
-		$login_array = array('AccessKeyId'=>$this->access_key,
-				'SecretAccessKey'=>$this->secret_key
-			);
-		$login_json = json_encode($login_array);
-
-
-
-	}
 }
 
 
